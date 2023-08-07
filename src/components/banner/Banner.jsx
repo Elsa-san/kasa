99cfd1fd6c97c292615d2aa9bdc3c097a4fae34e
Elsa-san <elsa.tartary@gmail.com>
2023-08-07 15:43:18 +0200

    gallery components

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -2,6 +2,8 @@ import "./banner.scss"
 
 export default function Banner() {
     return(
-        <section className="banner"></section>
+        <section className="banner">
+        <h1 className="banner-text">Chez vous, partout ou ailleurs</h1>
+        </section>
     )
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 import Banner from '../../components/banner/Banner'
+import Gallery from '../../components/gallery/Gallery'
 
 export default function Home() {
 	return (
 		<div>
             <Header />
             <Banner />
+            <Gallery />
             <Footer/>
 		</div>
 	)
