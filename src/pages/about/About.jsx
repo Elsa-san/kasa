aee5bad85dca7f9ade7edec45e3df937c1d7d0da
Elsa-san <elsa.tartary@gmail.com>
2023-08-07 17:03:35 +0200

    router fix

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,15 @@
+import Header from "../../components/header/Header"
+import Footer from "../../components/footer/Footer"
+import Banner from "../../components/banner/Banner"
+
+
 export default function Accomodation() {
 	return (
 		<div>
+        <Header />
+        <Banner />
             <h1>Ceci est la future page A propos</h1>
+        <Footer />
 		</div>
 	)
 }
--- src/pages/error/Error.jsx
+++ src/pages/error/Error.jsx
@@ -1,7 +1,16 @@
+import Header from "../../components/header/Header"
+import Footer from "../../components/footer/Footer"
+import "./error.scss"
+import {Link} from 'react-router-dom'
+
 export default function Error() {
 	return (
 		<div>
-            <h1>Ceci est la page d'erreur</h1>
+            <Header />
+            <h1 className="error">404</h1>
+            <p className="error-text">Oups! La page que vous recherchez n'existe pas</p>
+            <Link className="error-back" to="/">Retourner à la page d'accueil</Link>
+            <Footer />
 		</div>
 	)
 }
