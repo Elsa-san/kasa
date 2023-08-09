39d9b99ecb93028f586716b2acca11b05c0b512b
Elsa-san <elsa.tartary@gmail.com>
2023-08-09 15:55:06 +0200

    media queries

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -4,6 +4,7 @@ import {Link} from 'react-router-dom'
 export default function Card({id, title, cover}) {
     return (
         <Link to={`/accomodation/${id}`} className="card">
+            <div className='overlay'></div>
             <h3>{title}</h3>
             <img src={cover}alt={title}/>
 
