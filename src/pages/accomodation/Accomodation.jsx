263ed917e49ac7d54a7a6d5730067dac180d5145
Elsa-san <elsa.tartary@gmail.com>
2023-08-21 14:35:18 +0200

    accomodation stars

--- src/pages/accomodation/Accomodation.jsx
+++ src/pages/accomodation/Accomodation.jsx
@@ -3,13 +3,16 @@ import Header from "../../components/header/Header"
 import Footer from "../../components/footer/Footer"
 import Collapse from "../../components/collapse/Collapse"
 import datas from "../../datas/datas"
-import { useParams } from "react-router-dom";
+import { useParams } from "react-router-dom"
+import ActiveStar from "../../assets/star-active.png"
+import InactiveStar from "../../assets/star-inactive.png"
 
 
 export default function Accomodation() {
 
 const idAccomodation = useParams('id').id;
-	const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation);
+const dataCurrentAccomodation = datas.filter(data => data.id === idAccomodation)
+const rating = dataCurrentAccomodation[0].rating
 	
 	return (
 		<div>
@@ -18,12 +21,20 @@ const idAccomodation = useParams('id').id;
 			<h1>{dataCurrentAccomodation[0].title}</h1>
 			<p>{dataCurrentAccomodation[0].location}</p>
 			<div>
-							{dataCurrentAccomodation[0].tags.map((tag, index) => {
-								return (
-									<button className="tag" key={index}>{tag}</button>
-								)
-							})}
-						</div>
+			{dataCurrentAccomodation[0].tags.map((tag, index) => {
+			return (
+				<button className="tag" key={index}>{tag}</button>
+				)
+			})}
+			</div>
+			<div className="stars"> {Array.from({ length: 5 }).map((_, index) => (
+             <img
+             key={index}
+             src={index < rating ? ActiveStar : InactiveStar}
+             alt="star"/>
+             ))}
+             </div>
+                        
 		<div className="container-collapse">
 			<Collapse className="accomodation-collapse" title={"Description"} content={datas.description}/>
 			<Collapse title={"Equipements"} content={datas.equipments}/>
